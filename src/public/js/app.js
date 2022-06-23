b3db5538a37b2986187b6d4401d37b140a595fef
Chriso004 <qudcksrlf1@gmail.com>
2022-06-23 14:29:04 +0900

    chat function added

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,31 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("Form")
+const host = window.location.host;
+
+// socket은 서버로의 연결을 뜻함
+const socket = new WebSocket(`ws://${host}`);
+
+socket.addEventListener("open", () => {
+    console.log("connected");
+    console.log(host);
+});
+
+socket.addEventListener("close", () => {
+    console.log("disconnected from server");
+})
+
+socket.addEventListener("message", (message) => {
+    console.log(`from server : ${message.data}`);
+})
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = document.getElementById("msg_input");
+    const output = document.getElementById("msg_output");
+
+    output.value = input.value;
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -8,9 +8,23 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/public/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`listening on https://localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const ws_server = new WebSocket.Server( {server} );
+const wsServer = new WebSocket.Server( {server} );
+
+const sockets = []
+
+// socket은 서버와 연결된 브라우저
+wsServer.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log(socket);
+    console.log("connected");
+    socket.on("message", (message) => {
+        sockets.forEach((temp) => temp.send(message.toString()));
+    })
+});
+
+server.listen(3000, handleListen)
