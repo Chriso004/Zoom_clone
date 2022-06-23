4be5ecb821cc16f77449c71c2e5022c5e004ba17
Chriso004 <qudcksrlf1@gmail.com>
2022-06-23 10:13:16 +0900

    websocket server init

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,5 +10,7 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`listening on https://localhost:3000`);
-
 app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const ws_server = new WebSocket.Server( {server} );
